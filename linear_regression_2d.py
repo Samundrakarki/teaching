77b950dcdd04164d82cc1a5df4ea33d25f2f6653
Peter Zaspel <p.zaspel@jacobs-university.de>
2020-02-26 11:09:15 +0100

    * corrected size of interactive plot for 2d linear regression

--- linear_regression_2d.py
+++ linear_regression_2d.py
@@ -11,7 +11,7 @@ y = np.array([0, 0.3, 0.75, 1, 2])
 lin_regr = linear_model.LinearRegression()
 lin_regr.fit(X, y)
 
-fig = plt.figure(figsize=(8, 6))
+fig = plt.figure(figsize=(6, 5))
 ax = Axes3D(fig, elev=45, azim=-120)
 ax.scatter(X[:, 0], X[:, 1], y, c='r', marker='o', label='training set')
 ax.plot_surface(np.array([[-1, -1], [5, 5]]), np.array([[-1, 5], [-1, 5]]), lin_regr.predict(np.array([[-1, -1, 5, 5], [-1, 5, -1, 5]]).T).reshape((2, 2)), alpha=.5, label='linear regression predictor')
